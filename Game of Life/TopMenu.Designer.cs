21f98b48776a7676c4c196549a9bbcc3bb86b653
Vincent Chan <chanv@rohan.sdsu.edu>
2015-11-06 08:41:17 -0800

    initial push
    ayy

---
+++ Game of Life/TopMenu.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Game_of_Life
+{
+    partial class TopMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ayyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ayyToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ayyToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(93, 26);
+            // 
+            // ayyToolStripMenuItem
+            // 
+            this.ayyToolStripMenuItem.Name = "ayyToolStripMenuItem";
+            this.ayyToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.ayyToolStripMenuItem.Text = "ayy";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ayyToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(409, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ayyToolStripMenuItem1
+            // 
+            this.ayyToolStripMenuItem1.Name = "ayyToolStripMenuItem1";
+            this.ayyToolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+            this.ayyToolStripMenuItem1.Text = "ayy";
+            // 
+            // TopMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 299);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "TopMenu";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.TopMenu_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopMenu_MouseDown);
+            this.Resize += new System.EventHandler(this.TopMenu_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ayyToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ayyToolStripMenuItem1;
+    }
+}
+
---
+++ Game of Life/TopMenu.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_of_Life
+{
+    public partial class TopMenu : Form
+    {
+        Rectangle outer_rect;
+        Pen pen = new Pen(Color.Black, 1);
+        Brush borderBrush = new SolidBrush(Color.Black);
+        Random rand = new Random();
+
+        GameSquare[,] gameBoard = new GameSquare[10,10];
+
+        int squareWidth;
+        int squareHeight;
+
+        struct GameSquare
+        {
+            public GameSquare(int x, int y, int width, int height)
+            {
+                square = new Rectangle(x, y, width, height);
+                populated = false;
+                willBePopulated = false;
+            }
+            public Rectangle square;
+            public bool populated;
+            public bool willBePopulated;
+
+        }
+
+        public TopMenu()
+        {
+            InitializeComponent();
+            setBoard();
+        }
+
+        private void setBoard()
+        {
+            squareWidth = ClientSize.Width / 10;
+            squareHeight = (ClientSize.Height-24) / 10;
+            int stepW = ClientSize.Width / 10;
+            int stepH = (ClientSize.Height-24) / 10;
+            for (int row = 0; row < 10; row++)
+                for (int column = 0; column < 10; column++)
+                    gameBoard[row, column] = new GameSquare(column * stepW, (row*stepH)+24, squareWidth, squareHeight);
+            
+        }
+
+        private void TopMenu_MouseDown(object sender, MouseEventArgs e)
+        {
+            Invalidate();
+            Update();
+        }
+
+        private void TopMenu_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            foreach (GameSquare area in gameBoard) g.DrawRectangle(pen, area.square);
+        }
+
+        private void TopMenu_Resize(object sender, EventArgs e)
+        {
+            setBoard();
+            Invalidate();
+        }
+    }
+}
